230a9fe031dab30861448bed58f1798c5aa2b93a
Shreyas Ashok <Shreyas.Ashok@sas.com>
2017-06-07 21:07:11 +0100

    Google sheets testing

---
+++ gspreadsheet_test.py
@@ -0,0 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+ 
+ 
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+ 
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Carnatic Concert Recording Archive").get_worksheet(1)
+ 
+# Extract and print all of the values
+list_of_hashes = sheet.get_all_values()
+print(list_of_hashes)
+last_row = len(list_of_hashes)
+row = ["testtime", str(datetime.now())]
+sheet.insert_row(row, 2)
